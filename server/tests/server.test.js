a9ca9039ff6363e73af00c858653ba8488b8d23c
Balint Mogyorosi <mogyorosibalin@gmail.com>
2019-01-30 15:05:55 +0100

    Test POST /todos route

---
+++ server/tests/server.test.js
@@ -0,0 +1,51 @@
+const expect = require('expect');
+const request = require('supertest');
+
+const { app } = require('./../server');
+const { Todo } = require('./../models/todo')
+
+beforeEach((done) => {
+    Todo
+        .remove({})
+        .then(() => done());
+});
+
+describe('POST /todos', () => {
+    it('should create a new todo', (done) => {
+        const text = 'Test todo text';
+        request(app)
+            .post('/todos')
+            .send({ text })
+            .expect(200)
+            .expect((response) => {
+                expect(response.body.text).toBe(text);
+            })
+            .end((error, response) => {
+                if (error) return done(error);
+                Todo
+                    .find()
+                    .then((todos) => {
+                        expect(todos.length).toBe(1);
+                        expect(todos[0].text).toBe(text);
+                        done();
+                    })
+                    .catch(error => done(error));
+            });
+    });
+    it('should not create todo without body', (done) => {
+        request(app)
+            .post('/todos')
+            .send({})
+            .expect(400)
+            .end((error, response) => {
+                if (error) return done(error);
+                Todo
+                    .find()
+                    .then((todos) => {
+                        expect(todos.length).toBe(0);
+                        done();
+                    })
+                    .catch(error => done(error));
+            })
+    });
+});
