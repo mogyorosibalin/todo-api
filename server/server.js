18f38fbcc452043cf118a1f40d67e34c85a2d6cc
Balint Mogyorosi <mogyorosibalin@gmail.com>
2019-01-30 15:45:47 +0100

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { ObjectID } = require('mongodb');
 
 const { mongoose } = require('./db/mongoose');
 
@@ -9,33 +10,43 @@ const { User } = require('./models/user');
 const app = express();
 app.use(bodyParser.json());
 
-app.post('/todos', (request, response) => {
+app.post('/todos', (req, res) => {
     const todo = new Todo({
-        text: request.body.text
+        text: req.body.text
     });
     todo.save()
         .then((todo) => {
-            response.send(todo);
+            res.send(todo);
         })
         .catch((error) => {
-            response
-                .status(400)
-                .send(error);
+            res.status(400).send(error);
         });
 });
 
-app.get('/todos', (request, response) => {
+app.get('/todos', (req, res) => {
     Todo
         .find()
         .then((todos) => {
-            response.send({ todos });
+            res.send({ todos });
         })
         .catch((error) => {
-            response
-                .status(400)
-                .send(error);
+            res.status(400).send(error);
         });
-})
+});
+
+app.get('/todos/:id', (req, res) => {
+    const id = req.params.id;
+    if (!ObjectID.isValid(id)) res.status(404).send();
+    Todo
+        .findById(id)
+        .then((todo) => {
+            if (!todo) return res.status(404).send();
+            res.send({ todo });
+        })
+        .catch((error) => {
+            res.status(400).send();
+        });
+});
 
 app.listen(3000, () => {
     console.log('Started on port 3000');
