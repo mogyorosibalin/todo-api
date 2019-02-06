770d5cba8ab835397684577a65a146226b4d71bd
Balint Mogyorosi <mogyorosibalin@gmail.com>
2019-02-06 11:38:04 +0100

    Add POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -74,6 +74,25 @@ UserSchema.statics = {
                 'tokens.token': token,
                 'tokens.access': 'auth'
             });
+    },
+    findByCredentials: function(email, password) {
+        const User = this;
+        return User
+            .findOne({ email })
+            .then((user) => {
+                if (!user) {
+                    return Promise.reject();
+                }
+                return new Promise((resolve, reject) => {
+                    bcrypt.compare(password, user.password, (err, res) => {
+                        if (res) {
+                            resolve(user);
+                        } else {
+                            reject();
+                        }
+                    });
+                });
+            });
     }
 };
 
