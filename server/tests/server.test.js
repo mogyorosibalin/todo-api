1f58e398aec2c6a3411fdcfcae1dc22ce463cd10
Balint Mogyorosi <mogyorosibalin@gmail.com>
2019-02-06 13:10:25 +0100

    Add tests for DELETE /users/me/token

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -281,4 +281,23 @@ describe('POST /users/login', () => {
                 .catch((error) => done(error));
         });
     });
-})
+});
+
+describe('DELETE /users/me/token', () => {
+    it('should remove auth token on logout', (done) => {
+        request(app)
+            .delete('/users/me/token')
+            .set('x-auth', users[0].tokens[0].token)
+            .expect(200)
+            .end((err, res) => {
+                if (err) return err;
+                User
+                    .findById(users[0]._id)
+                    .then((user) => {
+                        expect(user.tokens.length).toBe(0);
+                        done();
+                    })
+                    .catch((e) => done(e));
+            });
+    });
+});
